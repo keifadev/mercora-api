1d0a28a9c8c6f9e7a0fd6da02c942c2df70b9c38
keifadev <joaojosafa11@gmail.com>
2026-07-24 21:38:21 -0300

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/keifadev/mercora_api/MercoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.keifadev.mercora_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercoraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keifadev/mercora_api/MercoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keifadev.mercora_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
