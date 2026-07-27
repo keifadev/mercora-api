1fe2f8b5cd473349d76792e423bc103e9d01efb6
keifadev <joaojosafa11@gmail.com>
2026-07-27 13:42:10 -0300

    feat: add Client entity

---
+++ src/main/java/com/keifadev/mercora/model/Client.java
@@ -0,0 +1,27 @@
+package com.keifadev.mercora.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "client")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+}
