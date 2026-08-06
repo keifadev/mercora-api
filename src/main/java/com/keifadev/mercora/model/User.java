1f47977313cc29a0a138b4ce222df72d78a72a9d
keifadev <joaojosafa11@gmail.com>
2026-08-06 20:58:23 -0300

    feat: add User entity

---
+++ src/main/java/com/keifadev/mercora/model/User.java
@@ -0,0 +1,35 @@
+package com.keifadev.mercora.model;
+
+import com.keifadev.mercora.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
